e22f673e2b26bff4ebddd892c937099b72fab7ab
范祎楠 <>
2022-09-21 14:43:35 +0800

    修复未锁定屏幕方向时旋转失效的bug

--- OrientationTest/ViewController.mm
+++ OrientationTest/ViewController.mm
@@ -10,6 +10,8 @@
 
 @interface ViewController () <UITextFieldDelegate>
 @property (nonatomic, strong) UIButton *testView;
+@property (nonatomic, assign) NSUInteger orientation;
+@property (nonatomic, assign) BOOL isRotate;
 @end
 
 @implementation ViewController
@@ -22,24 +24,43 @@
     _testView.backgroundColor = UIColor.redColor;
     [_testView addTarget:self action:@selector(test) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_testView];
+    _orientation = UIInterfaceOrientationMaskPortrait;
+    _isRotate = NO;
 }
 
 - (void)test
 {
-    UITextField *textField = [[UITextField alloc] init];
-    [self.view addSubview:textField];
-    [textField becomeFirstResponder];
-    [textField resignFirstResponder];
-    [textField removeFromSuperview];
+    //强制调一次AppDelegate supportedInterfaceOrientationsForWindow，解决调试转屏失败的bug
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        UITextField *textField = [[UITextField alloc] init];
+        [UIApplication.sharedApplication.keyWindow addSubview:textField];
+        [textField becomeFirstResponder];
+        [textField resignFirstResponder];
+        [textField removeFromSuperview];
+    });
     
-    NSNumber *value=[NSNumber numberWithInt:UIInterfaceOrientationLandscapeRight];
+    //防止未锁定屏幕方向时旋转失效
+    NSNumber *value=[NSNumber numberWithInt:UIInterfaceOrientationUnknown];
     [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
+    
+    if (_isRotate) {
+        _orientation = UIInterfaceOrientationMaskPortrait;
+        NSNumber *value=[NSNumber numberWithInt:UIInterfaceOrientationPortrait];
+        [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
+    } else {
+        _orientation = UIInterfaceOrientationMaskLandscapeRight;
+        NSNumber *value=[NSNumber numberWithInt:UIInterfaceOrientationLandscapeRight];
+        [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
+    }
+    _isRotate = !_isRotate;
 
+    NSLog(@"touch");
 }
 
 -(NSUInteger)supportedInterfaceOrientations
 {
-    return UIInterfaceOrientationMaskAll;
+    return _orientation;
 }
 //-------------------------------------------------------------------------------
 - (BOOL)shouldAutorotate
